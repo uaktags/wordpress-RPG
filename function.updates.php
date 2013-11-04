fb2c042d5ac7e605d8463a8fc953e7768af679b8
uaktags <timgarrity89@gmail.com>
2013-11-03 20:00:37 -0500

    Update function.updates.php

--- function.updates.php
+++ function.updates.php
@@ -10,7 +10,7 @@ if (is_admin()) { // note the use of is_admin() to double check that this is hap
         'raw_url' => 'https://raw.github.com/uaktags/wordpress-RPG/master', // the github raw url of your github repo
         'github_url' => 'https://github.com/uaktags/wordpress-RPG', // the github url of your github repo
         'zip_url' => 'https://github.com/uaktags/wordpress-RPG/zipball/master', // the zip url of the github repo
-        'sslverify' => true, // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
+        'sslverify' => false, // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
         'requires' => '3.5', // which version of WordPress does your plugin require?
         'tested' => '3.6', // which version of WordPress is your plugin tested up to?
         'readme' => 'readme.md', // which file to use as the readme for the version number
