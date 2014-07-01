97457dc52160fc3abcb9384e777bbe92fe8cb935
JDGrimes <jdg@codesymphony.co>
2014-07-01 16:43:59 -0400

    Avoid an error when installing
    When the latest version of the plugin is first activated, it will give
    an error:
    
    `
    [01-Jul-2014 20:08:06 UTC] WordPress database error Table
    'wp_dev_1.wptrunk_rpg_usermeta' doesn't exist for query select * from
    wptrunk_rpg_usermeta made by activate_plugin,
    include_once('/plugins/wprpg/wordpress-rpg.php'), transition_db
    `
    
    This is because the database transition function is called, and it
    attempts to transition the database, even though the old database
    doesn’t exist (because this is a fresh install, not an update from an
    old version).
    
    I’ve made the following changes:
    
    - Renamed `transition_db()` to `wpRPG_transition_db()`, to avoid
    possible conflicts if another plugin has a function with the same name.
    - Added a check to the transition function to see if the old database
    exists, and only attempting the transition if it does.
    
    Note that this code is all untested, I’ll leave that to you. :)
    
    Also, I don’t think this is actually the _best_ solution. Ideally, it
    could be changed in the following ways:
    
    - The option stored in the database wouldn’t be a 0/1 on/off value.
    Instead you could store an option that would hold the database version.
    That way if you make more changes in the future, you won’t have to add
    a new option each time. Instead, you could check a single option
    storing the current DB version, and run the updates needed for newer
    versions.
    - The database version option would be added with an initial value
    (maybe the version of the plugin) when it is activated.
    - The transition/update would need to run later in code execution,
    hooked to an action like `’admin_init’`. That would be needed so that
    the activation code could set the initial database version before the
    transition is attempted. Otherwise you’d get an empty database version
    from the option in the transition function.

--- wordpress-rpg.php
+++ wordpress-rpg.php
@@ -30,10 +30,7 @@ function wprpg_plugin_autoload($class_name) {
 
 if (!get_option('wpRPG_transition_db')){
 	add_option('wpRPG_transition_db', 1, '', 'yes');
-	transition_db();
-	//wp_die('Less than/equal to 1.0.13 and never been transitioned');
-}else{
-	
+	wpRPG_transition_db();
 }
 
 spl_autoload_register('wprpg_plugin_autoload');
@@ -56,21 +53,30 @@ include ( __DIR__.'/wprpg-library.php');
 register_activation_hook( __FILE__, array( $rpg, 'wpRPG_on_activation' ) );
 register_deactivation_hook( __FILE__, array( $rpg, 'wpRPG_on_deactivation' ) );
 
+/**
+ * Updates the database schema if it is an old version.
+ */
+function wpRPG_transition_db() {
 
-function transition_db(){
 	global $wpdb;
-	$sql = 'select * from '. $wpdb->base_prefix . 'rpg_usermeta';
-	$res = $wpdb->get_results($sql);
-	foreach ( $res as $usermeta )
-	{
-		update_user_meta($usermeta->pid, 'gold', $usermeta->gold);
-		update_user_meta($usermeta->pid, 'xp', $usermeta->xp);
-		update_user_meta($usermeta->pid, 'hp', $usermeta->hp);
-		update_user_meta($usermeta->pid, 'strength', $usermeta->strength);
-		update_user_meta($usermeta->pid, 'defense', $usermeta->defense);
-		update_user_meta($usermeta->pid, 'bank', $usermeta->bank);
-		update_user_meta($usermeta->pid, 'last_active', $usermeta->race);
-		update_user_meta($usermeta->pid, 'race', $usermeta->race);
+
+	// The meta was stored in a special table before 1.0.13.
+	$table_name = $wpdb->base_prefix . 'rpg_usermeta';
+
+	if ( $table_name === $wpdb->get_var( $wpdb->prepare( 'SHOW TABLES LIKE %s', $table_name ) ) {
+		$sql = 'select * from ' . $table_name;
+		$res = $wpdb->get_results($sql);
+		foreach ( $res as $usermeta )
+		{
+			update_user_meta($usermeta->pid, 'gold', $usermeta->gold);
+			update_user_meta($usermeta->pid, 'xp', $usermeta->xp);
+			update_user_meta($usermeta->pid, 'hp', $usermeta->hp);
+			update_user_meta($usermeta->pid, 'strength', $usermeta->strength);
+			update_user_meta($usermeta->pid, 'defense', $usermeta->defense);
+			update_user_meta($usermeta->pid, 'bank', $usermeta->bank);
+			update_user_meta($usermeta->pid, 'last_active', $usermeta->race);
+			update_user_meta($usermeta->pid, 'race', $usermeta->race);
+		}
 	}
 }
 ?>
